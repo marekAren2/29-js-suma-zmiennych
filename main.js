9478de976f7697c37518a9dde06d5dda8beafe98
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-13 11:15:06 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+/* 
+1. Stwórz dokument HTML. Stwórz plik main.js w odpowiednim folderze i podepnij go pod plik HTML.
+
+2. W pliku main.js zdeklaruj dwie zmienne o nazwie:
+- "firstNumber"
+- "secondNumber"
+
+3. Przypisz do nich dowolne liczby.
+4. Następnie napisz funkcję, która przyjmuje dwa parametry - 2 liczby.
+5. Funkcja ma zwracać (return) sumę tych parametrów.
+
+Pliki wgraj do repozytorium zdalnego i podeślij link do sprawdzenia.
+
+Podpowiedź:
+- Wewnątrz funkcji zdeklaruj zmienną (np. "suma"), która będzie sumą dwóch przekazywanych parametrów. Następnie zwróć tę zmienną.
+- Pamiętaj aby parametry nazywały się inaczej niż nazwy Twoich zmiennych
+ */
